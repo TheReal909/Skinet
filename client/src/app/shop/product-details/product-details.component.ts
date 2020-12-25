b743bd4ae6d9681a5c42471e64fbbd59d828ccee
TheReal909 <hadybah@outlook.com>
2020-12-25 17:44:01 +0000

    End of section 12 and starting the basket section

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -13,7 +13,9 @@ export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
   constructor(private shopService: ShopService,
-              private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) { }
+              private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+                this.bcService.set('@productsDetails', '');
+               }
 
   ngOnInit() {
     this.loadProduct();
